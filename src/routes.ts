f19b5756a055ff08cb53b9ef7cae98dde906cdc3
Simon Erik Nyblom <nyblomsimon@gmail.com>
2026-02-02 10:07:48 +0100

    fix: added a more specific route

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,7 @@
 import { Router, Request, Response } from "express";
 const router: Router = Router();
 
-router.get("/", (req: Request, res: Response) => {
+router.get("/api", (req: Request, res: Response) => {
   res.status(200).send("API is working");
 });
 
